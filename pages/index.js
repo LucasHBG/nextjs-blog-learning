761dc6393f89a77be6a3960f8b0df836a7b1f786
Lucas <lukahbg@gmail.com>
2021-02-11 22:23:33 -0300

    Pre-rendering and data fetching done

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from "next/head";
 import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -32,6 +42,22 @@ function Home() {
           Spring, Hibernate and Maven during the development of the
           administrative system already mentioned above.
         </p>
+      </section>
+
+      {/* Section with posts sorted by date */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
---
+++ posts/comon-text.md
@@ -0,0 +1,6 @@
+---
+title: 'What Software Engineers do'
+date: '2020-01-03'
+---
+
+As a software engineer you have to work on different roles throught the entire time you're there. 
